ab05a6ad004adb68ff204f37ff6fdac256ca132b
Mohanned Ahmed EL-Sayed <91846345+MohannedAhmed67@users.noreply.github.com>
2023-12-16 16:09:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ import pandas as pd
 from datetime import datetime
 import base64
 import io
+import os
 
 app = Flask(__name__)
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
-cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath)
+file_path = 'trainer\\trainer.yml'
+if os.path.exists(file_path):
+    recognizer.read(file_path)
+else:
+    print("Error reading the file 1")
+
+cascadePath = 'haarcascade_frontalface_default.xml'
+if os.path.exists(cascadePath):
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+else:
+    print("Error reading the file 2")
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-# Initiate some global variables
+# Initiate some global variable
 data = {'Name': [], 'Time': [], 'Id': []}
 id_names = {0: 'None', 1: 'Arwa', 2: 'Mariam', 3: 'Toqa'}  # Replace with your actual IDs and names
 
@@ -26,18 +36,25 @@ def recognize_faces(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
 
+    unique_names = set()  # Use a set to track unique names
+
     for (x, y, w, h) in faces:
         id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
         if confidence < 100:
             name = id_names[id]
-            confidence = round(100 - confidence)
-            data['Name'].append(name)
-            data['Time'].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            data['Id'].append(id)  # Store the recognized face ID
 
-            # Draw the recognized name and confidence on the frame
-            cv2.putText(img, f"{name} ({confidence}%)", (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            # Check if the name is already in the set
+            if name not in unique_names:
+                unique_names.add(name)
+
+                confidence = round(100 - confidence)
+                data['Name'].append(name)
+                data['Time'].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                data['Id'].append(id)  # Store the recognized face ID
+
+                # Draw the recognized name and confidence on the frame
+                cv2.putText(img, f"{name} ({confidence}%)", (x + 5, y - 5), font, 1, (255, 255, 255), 2)
 
     return img
 
@@ -57,6 +74,37 @@ def take_attendance():
 
         df = pd.DataFrame(data)
         df.to_excel('attendance.xlsx', index=False)
+
+        # Clear the data dictionary after processing
+        data['Name'].clear()
+        data['Time'].clear()
+        data['Id'].clear()
+
+        return jsonify({'status': 'success', 'names': recognized_names})
+
+    return jsonify({'status': 'error', 'message': 'No image data received'})
+
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    image_data = request.json.get('image', '')
+
+    if image_data:
+        image_data = base64.b64decode(image_data.split(',')[1])
+        img_array = np.frombuffer(image_data, dtype=np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+        img = recognize_faces(img)
+
+        recognized_names = [id_names[id] for id in data['Id']]
+
+        df = pd.DataFrame(data)
+        df.to_excel('attendance.xlsx', index=False)
+
+        # Clear the data dictionary after processing
+        data['Name'].clear()
+        data['Time'].clear()
+        data['Id'].clear()
 
         return jsonify({'status': 'success', 'names': recognized_names})
 
