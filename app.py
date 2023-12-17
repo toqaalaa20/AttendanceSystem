d2c05798a9f37b85b9b9361812b55c51620f53b6
Heakl <ahmed.heakl@ejust.edu.eg>
2023-12-17 17:28:29 +0200

    Refactored main.py and added generation script

--- app.py
+++ app.py
@@ -1,103 +1,111 @@
+"""Main script for the Flask web application."""
+from datetime import datetime
+import base64
+import os
+
 from flask import Flask, render_template, request, jsonify
 import cv2
 import numpy as np
 import pandas as pd
-from datetime import datetime
-import base64
-import io
-import os
 
 app = Flask(__name__)
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-file_path = 'trainer\\trainer.yml'
-if os.path.exists(file_path):
-    recognizer.read(file_path)
+TRAINER_PATH = "trainer.yml"
+if os.path.exists(TRAINER_PATH):
+    recognizer.read(TRAINER_PATH)
 else:
-    print("Error reading the file 1")
+    print(f"Cannot find file {TRAINER_PATH}")
 
-cascadePath = 'haarcascade_frontalface_default.xml'
-if os.path.exists(cascadePath):
-    faceCascade = cv2.CascadeClassifier(cascadePath)
+CASCADE_WEIGHTS_PATH = "haarcascade_frontalface_default.xml"
+if os.path.exists(CASCADE_WEIGHTS_PATH):
+    faceCascade = cv2.CascadeClassifier(CASCADE_WEIGHTS_PATH)
 else:
-    print("Error reading the file 2")
+    print(f"Cannot find file {CASCADE_WEIGHTS_PATH}")
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-# Initiate some global variable
 unique_names = set()  # Use a set to track unique names
-data = {'Name': [], 'Time': [], 'Id': []}
-id_names = {0: 'None', 1: 'Arwa', 2: 'Mariam', 3: 'Toqa'}  # Replace with your actual IDs and names
+data = {"Name": [], "Time": [], "Id": []}
+id_to_names = {
+    0: "None",
+    1: "Arwa",
+    2: "Mariam",
+    3: "Toqa",
+}
+ATTENDANCE_SHEET_PATH = "attendance.xlsx"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    """Index endpoint for the Flask web application."""
+    return render_template("index.html")
 
-def recognize_faces(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
 
-    for (x, y, w, h) in faces:
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+def _recognize_faces(img: np.ndarray) -> np.ndarray:
+    """Recognize faces in the given image and return the image with names.
 
-        if confidence < 100:
-            name = id_names[id]
+    Args:
+        img: Image to recognize faces in.
 
-            # Check if the name is already in the set
-            if name not in unique_names:
-                unique_names.add(name)
+    Returns:
+        Image with names of recognized faces written on top.
+    """
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray_img, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30)
+    )
 
-                confidence = round(100 - confidence)
-                data['Name'].append(name)
-                data['Time'].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-                data['Id'].append(id)  # Store the recognized face ID
+    for x, y, w, h in faces:
+        face_id, distance = recognizer.predict(gray_img[y : y + h, x : x + w])
+        if distance >= 100:
+            continue
+        name = id_to_names[face_id]
 
-                # Draw the recognized name and confidence on the frame
-                cv2.putText(img, f"{name} ({confidence}%)", (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        if name in unique_names:
+            continue
+
+        unique_names.add(name)
+
+        confidence = round(100 - distance)
+        data["Name"].append(name)
+        data["Time"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        data["Id"].append(face_id)
+
+        cv2.putText(
+            img=img,
+            text=f"{name} ({confidence}%)",
+            org=(x + 5, y - 5),
+            fontFace=font,
+            fontScale=1,
+            color=(255, 255, 255),
+            thickness=2,
+        )
 
     return img
 
-@app.route('/take_attendance', methods=['POST'])
-def take_attendance():
-    image_data = request.json.get('image', '')
 
-    if image_data:
-        # Decode base64 image data
-        image_data = base64.b64decode(image_data.split(',')[1])
-        img_array = np.frombuffer(image_data, dtype=np.uint8)
-        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
-        img = recognize_faces(img)
-
-        recognized_names = [id_names[id] for id in data['Id']]
-
-        df = pd.DataFrame(data)
-        df.to_excel('attendance.xlsx', index=False)
-
-        return jsonify({'status': 'success', 'names': recognized_names})
-
-    return jsonify({'status': 'error', 'message': 'No image data received'})
-
-
-@app.route('/upload_image', methods=['POST'])
+@app.route("/upload_image", methods=["POST"])
 def upload_image():
-    image_data = request.json.get('image', '')
+    """Upload image endpoint for the Flask web application."""
+    image_data = request.json.get("image", "")
 
-    if image_data:
-        image_data = base64.b64decode(image_data.split(',')[1])
-        img_array = np.frombuffer(image_data, dtype=np.uint8)
-        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    if not image_data:
+        return jsonify({"status": "error", "message": "No image data received"})
 
-        img = recognize_faces(img)
+    image_data = base64.b64decode(image_data.split(",")[1])
+    img_array = np.frombuffer(image_data, dtype=np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-        recognized_names = [id_names[id] for id in data['Id']]
+    img = _recognize_faces(img)
 
-        df = pd.DataFrame(data)
-        df.to_excel('attendance.xlsx', index=False)
+    recognized_names = [id_to_names[id] for id in data["Id"]]
 
-        return jsonify({'status': 'success', 'names': recognized_names})
+    df = pd.DataFrame(data)
+    df.to_excel(ATTENDANCE_SHEET_PATH, index=False)
 
-    return jsonify({'status': 'error', 'message': 'No image data received'})
+    return jsonify({"status": "success", "names": recognized_names})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
